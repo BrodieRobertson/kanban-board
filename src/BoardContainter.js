daa6487c94adb30c7dba743a9d590cc8c4fab36e
Brodie Robertson <brodierobertson54321@gmail.com>
2019-12-08 12:45:34 +1030

    Add support for file upload

--- src/BoardContainter.js
+++ src/BoardContainter.js
@@ -9,18 +9,24 @@ const BoardContainer = () => {
   const [notes, setNotes] = useState([]);
 
   // Note Functions
+  /**
+   */
   function addNote(index) {
     var workingNotes = deepCopy(notes);
     workingNotes[index].notes.push(createNote("note", "new"));
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function editNote(noteIndex, boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].notes[noteIndex].edit = true;
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function saveNoteEdit(event, noteIndex, boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].notes[noteIndex].content = event.target.value;
@@ -28,12 +34,16 @@ const BoardContainer = () => {
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function deleteNote(noteIndex, boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].notes.splice(noteIndex, 1);
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function changeNoteColor(event, noteIndex, boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].notes[noteIndex].color = event.target.value;
@@ -41,22 +51,30 @@ const BoardContainer = () => {
   }
 
   // Board Functions
+  /**
+   */
   function addBoard() {
     setNotes([...notes, createBoard("New Board", [])]);
   }
 
+  /**
+   */
   function deleteBoard(boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes.splice(boardIndex, 1);
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function editBoard(boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].edit = true;
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function saveBoardEdit(event, boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].title = event.target.value;
@@ -64,23 +82,53 @@ const BoardContainer = () => {
     setNotes(workingNotes);
   }
 
+  /**
+   */
   function changeBoardColor(event, boardIndex) {
     var workingNotes = deepCopy(notes);
     workingNotes[boardIndex].color = event.target.value;
     setNotes(workingNotes);
   }
 
-  //setTimeout("create('Hello world!', 'myfile.txt', 'text/plain')");
+  // Data Functions
+  /**
+   */
   function create(text, type) {
     var file = new Blob([text], { type: type });
     return URL.createObjectURL(file);
   }
 
+  /**
+   */
   function downloadData() {
     let a = document.createElement("a");
-    a.href = create(JSON.stringify(notes), "text/plain");
+    a.href = create(JSON.stringify(notes), "application/json");
     a.download = "kanban-data.json";
     a.click();
+  }
+
+  /**
+   */
+  function uploadData(e) {
+    let file = e.target.files[0];
+
+    if (!file || file.type !== "application/json") {
+      window.alert("Invalid file type");
+      return;
+    } else {
+      const reader = new FileReader();
+      reader.onload = e => {
+        setNotes(JSON.parse(e.target.result));
+      };
+      reader.readAsText(file);
+    }
+  }
+
+  /**
+   */
+  function clickImport() {
+    let upload = document.getElementById("import-btn");
+    upload.click();
   }
 
   return (
@@ -110,9 +158,18 @@ const BoardContainer = () => {
       >
         New Board
       </button>
-      <button className="margin-bottom-small margin-right-small">
+      <button
+        onClick={clickImport}
+        className="margin-bottom-small margin-right-small"
+      >
         Import Data
       </button>
+      <input
+        id="import-btn"
+        type="file"
+        className="display-none"
+        onChange={e => uploadData(e)}
+      />
       <button
         onClick={downloadData}
         className="margin-bottom-small margin-right-small"
